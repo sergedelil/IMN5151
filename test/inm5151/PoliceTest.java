6f28afa43b472fa46d8f0f5fc90a0a21a89c41fa
olgamak <mapulache1@gmail.com>
2016-04-23 19:08:21 -0400

    Modification SoinAssureTest

--- test/inm5151/PoliceTest.java
+++ test/inm5151/PoliceTest.java
@@ -15,42 +15,49 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- *@author Serge Dogny
+ * @author Serge Dogny
  * @author ALLAMOU Fatima-Ezzahra
- * @author MAKHIVCHTCHOUK  Olga
+ * @author MAKHIVCHTCHOUK Olga
  */
 public class PoliceTest {
-    
+
     Police police1;
     Police police2;
     Police police3;
-    
+    SoinAssure soin1;
+    SoinAssure soin2;
+
     public PoliceTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
-        
+
         police1 = new Police("A");
         police2 = new Police();
         police3 = new Police("B");
-        
+        soin1 = new SoinAssure("100", 0.50, 50.0, 0.0);
+        soin2 = new SoinAssure("175", 0.9, 80.0, 0.0);
+        police3.ajouterSoinAssure(soin1);
+        police3.ajouterSoinAssure(soin2);
     }
-    
+
     @After
     public void tearDown() {
-        
+
         police1 = null;
         police2 = null;
         police3 = null;
+        soin1 = null;
+        soin2 = null;
     }
 
     /**
@@ -58,8 +65,8 @@ public class PoliceTest {
      */
     @Test
     public void testGetContrat() {
-        
-        assertEquals("A",police1.getContrat());
+
+        assertEquals("A", police1.getContrat());
         assertNull(police2.getContrat());
         assertEquals("B", police3.getContrat());
         assertFalse("C".equals(police1.getContrat()));
@@ -70,10 +77,11 @@ public class PoliceTest {
      */
     @Test
     public void testGetListeSoinsAssures() {
-        
-        assertEquals(0,police1.getListeSoinsAssures().size());
+
+        assertEquals(0, police1.getListeSoinsAssures().size());
         assertNotNull(police1.getListeSoinsAssures());
-        
+
+        assertEquals(2, police3.getListeSoinsAssures().size());
     }
 
     /**
@@ -81,15 +89,15 @@ public class PoliceTest {
      */
     @Test
     public void testSetContrat() {
-        
+
         police2.setContrat("E");
-        assertEquals("E", police2.getContrat()); 
-        
+        assertEquals("E", police2.getContrat());
+
         police2.setContrat("F");
         assertEquals("F", police2.getContrat());
-        
+
         assertFalse("G".equals(police2.getContrat()));
-        
+
     }
 
     /**
@@ -97,7 +105,7 @@ public class PoliceTest {
      */
     @Test
     public void testValiderContrat() {
-        
+
         assertTrue(police1.validerContrat());
         police2.setContrat("T");
         assertFalse(police2.validerContrat());
@@ -109,11 +117,11 @@ public class PoliceTest {
      */
     @Test
     public void testAjouterSoinAssure() {
-        
+
         SoinAssure soin = new SoinAssure("100", 0.50, 50.0, 0.0);
         police1.ajouterSoinAssure(soin);
         assertFalse(police1.getListeSoinsAssures().isEmpty());
-        assertEquals(1,police1.getListeSoinsAssures().size());
+        assertEquals(1, police1.getListeSoinsAssures().size());
     }
 
     /**
@@ -121,19 +129,18 @@ public class PoliceTest {
      */
     @Test
     public void testSupprimerSoinAssure() {
-        
+
         assertTrue(police1.getListeSoinsAssures().isEmpty());
-        SoinAssure soin = new SoinAssure("100", 0.50, 50.0,0.0);
+        SoinAssure soin = new SoinAssure("100", 0.50, 50.0, 0.0);
         police1.ajouterSoinAssure(soin);
         SoinAssure soin2 = new SoinAssure("175", 0.9, 150.0, 0.0);
         police1.ajouterSoinAssure(soin2);
-        assertEquals(2,police1.getListeSoinsAssures().size());
+        assertEquals(2, police1.getListeSoinsAssures().size());
         police1.supprimerSoinAssure(soin);
-        assertEquals(1,police1.getListeSoinsAssures().size());
+        assertEquals(1, police1.getListeSoinsAssures().size());
         police1.supprimerSoinAssure(soin2);
         assertTrue(police1.getListeSoinsAssures().isEmpty());
-        
-        
+
     }
-    
+
 }
--- test/inm5151/SoinAssureTest.java
+++ test/inm5151/SoinAssureTest.java
@@ -13,43 +13,56 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- *@author Serge Dogny
+ * @author Serge Dogny
  * @author ALLAMOU Fatima-Ezzahra
- * @author MAKHIVCHTCHOUK  Olga
+ * @author MAKHIVCHTCHOUK Olga
  */
 public class SoinAssureTest {
-    
+
     SoinAssure soin1;
     SoinAssure soin2;
     SoinAssure soin3;
     SoinAssure soin4;
-    
+    SoinAssure soin5;
+    SoinAssure soin6;
+    SoinAssure soin7;
+    SoinAssure soin8;
+
     public SoinAssureTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
-        soin1 = new SoinAssure("100", 0.50, 50.0,0.0);
+        soin1 = new SoinAssure("100", 0.50, 50.0, 0.0);
         soin2 = new SoinAssure("175", 0.9, 80.0, 0.0);
         soin3 = new SoinAssure();
         soin4 = new SoinAssure("800", 0.4, 60.5, 0.0);
+
+        soin5 = new SoinAssure("0", 0.4, 60.5, 0.0);
+        soin6 = new SoinAssure("400", 0.4, 60.5, 0.0);
+        soin7 = new SoinAssure("600", 0.4, 60.5, 300.0);
+        soin8 = new SoinAssure("500", 0.4, 60.5, 150.0);
     }
-    
+
     @After
     public void tearDown() {
-        
+
         soin1 = null;
         soin2 = null;
         soin3 = null;
         soin4 = null;
+        soin5 = null;
+        soin6 = null;
+        soin7 = null;
+        soin8 = null;
     }
 
     /**
@@ -57,12 +70,11 @@ public class SoinAssureTest {
      */
     @Test
     public void testGetNumSoin() {
-        
+
         assertEquals("100", soin1.getNumSoin());
         assertNull(soin3.getNumSoin());
         assertEquals("175", soin2.getNumSoin());
-        
-       
+
     }
 
     /**
@@ -70,10 +82,9 @@ public class SoinAssureTest {
      */
     @Test
     public void testGetPourcentage() {
-        
-        
-        assertEquals(0.50, soin1.getPourcentage(),0.01);
-        assertEquals(0.9, soin2.getPourcentage(),0.01);
+
+        assertEquals(0.50, soin1.getPourcentage(), 0.01);
+        assertEquals(0.9, soin2.getPourcentage(), 0.01);
     }
 
     /**
@@ -81,9 +92,9 @@ public class SoinAssureTest {
      */
     @Test
     public void testGetLimite() {
-        
-        assertEquals(50.0, soin1.getLimite(),0.01);
-        assertEquals(80.0, soin2.getLimite(),0.01);
+
+        assertEquals(50.0, soin1.getLimite(), 0.01);
+        assertEquals(80.0, soin2.getLimite(), 0.01);
     }
 
     /**
@@ -91,12 +102,12 @@ public class SoinAssureTest {
      */
     @Test
     public void testSetNumSoin() {
-        
+
         assertNull(soin3.getNumSoin());
         soin3.setNumSoin("200");
         assertNotNull(soin3.getNumSoin());
         assertEquals("200", soin3.getNumSoin());
-        
+
     }
 
     /**
@@ -104,10 +115,10 @@ public class SoinAssureTest {
      */
     @Test
     public void testSetPourcentage() {
-        
-        assertEquals(0.0,soin3.getPourcentage(),0.01);
+
+        assertEquals(0.0, soin3.getPourcentage(), 0.01);
         soin3.setPourcentage(0.7);
-        assertEquals(0.7,soin3.getPourcentage(),0.01);
+        assertEquals(0.7, soin3.getPourcentage(), 0.01);
     }
 
     /**
@@ -115,10 +126,10 @@ public class SoinAssureTest {
      */
     @Test
     public void testSetLimite() {
-        
-        assertEquals(0.0,soin3.getLimite(),0.01);
+
+        assertEquals(0.0, soin3.getLimite(), 0.01);
         soin3.setLimite(56.0);
-        assertEquals(56.0,soin3.getLimite(),0.01);
+        assertEquals(56.0, soin3.getLimite(), 0.01);
     }
 
     /**
@@ -126,7 +137,7 @@ public class SoinAssureTest {
      */
     @Test
     public void testValiderNumeroSoin() {
-        
+
         assertTrue(soin1.validerNumeroSoin());
         assertFalse(soin4.validerNumeroSoin());
         assertTrue(soin2.validerNumeroSoin());
@@ -137,7 +148,7 @@ public class SoinAssureTest {
      */
     @Test
     public void testValiderLaCouvertureDuSoin() {
-        
+
         assertTrue(soin1.validerLaCouvertureDuSoin());
         soin3.setPourcentage(2.0);
         assertFalse(soin3.validerLaCouvertureDuSoin());
@@ -151,10 +162,98 @@ public class SoinAssureTest {
      */
     @Test
     public void testValiderLaLimiteDuSoin() {
-        
+
         assertTrue(soin1.validerLaLimiteDuSoin());
         soin3.setLimite(-2.0);
         assertFalse(soin3.validerLaLimiteDuSoin());
         assertTrue(soin2.validerLaLimiteDuSoin());
+    }
+
+    /**
+     * Test of admetLimiteMensuelle method, of class SoinAssure.
+     */
+    @Test
+    public void testAdmetLimiteMensuelle() {
+
+        SoinAssure instance = soin5;
+        boolean expResult = false;
+        boolean result = instance.admetLimiteMensuelle();
+        assertEquals(expResult, result);
+
+        instance = soin6;
+        expResult = false;
+        result = instance.admetLimiteMensuelle();
+        assertEquals(expResult, result);
+
+        instance = soin7;
+        expResult = true;
+        result = instance.admetLimiteMensuelle();
+        assertEquals(expResult, result);
+
+        instance = soin8;
+        expResult = true;
+        result = instance.admetLimiteMensuelle();
+        assertEquals(expResult, result);
+
+    }
+
+    /**
+     * Test of getLimiteMensuelle method, of class SoinAssure.
+     */
+    @Test
+    public void testGetLimiteMensuelle() {
+        SoinAssure instance = soin7;
+        double expResult = 300.0;
+        double result = instance.getLimiteMensuelle();
+        assertEquals(expResult, result, 300.0);
+
+        instance = soin8;
+        expResult = 150.0;
+        result = instance.getLimiteMensuelle();
+        assertEquals(expResult, result, 150.0);
+
+        instance = soin5;
+        expResult = 0.0;
+        result = instance.getLimiteMensuelle();
+        assertEquals(expResult, result, 0.0);
+
+        instance = soin6;
+        expResult = 0.0;
+        result = instance.getLimiteMensuelle();
+        assertEquals(expResult, result, 0.0);
+
+        instance = soin8;
+        expResult = 0.0;
+        assertFalse(expResult == instance.getLimiteMensuelle());
+    }
+
+    /**
+     * Test of setLimiteMensuelle method, of class SoinAssure.
+     */
+    @Test
+    public void testSetLimiteMensuelle() {
+        double limiteMensuelle = 0.0;
+        SoinAssure instance = soin7;
+        instance.setLimiteMensuelle(limiteMensuelle);
+        double result = instance.getLimiteMensuelle();
+        assertEquals(limiteMensuelle, result, 0.0);
+
+        instance = soin7;
+        instance.setLimiteMensuelle(limiteMensuelle);
+        result = instance.getLimiteMensuelle();
+        assertEquals(limiteMensuelle, result, 0.0);
+
+        limiteMensuelle = 30.0;
+        instance = soin5;
+        instance.setLimiteMensuelle(limiteMensuelle);
+        result = instance.getLimiteMensuelle();
+        assertEquals(limiteMensuelle, result, 30.0);
+
+        limiteMensuelle = 66.0;
+        instance = soin6;
+        instance.setLimiteMensuelle(limiteMensuelle);
+        result = instance.getLimiteMensuelle();
+        assertEquals(limiteMensuelle, result, 66.0);
+
     }
 }
