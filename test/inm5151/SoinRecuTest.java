a962e7ef0ceec0826ac50d771a8593ce09da0158
olgamak <mapulache1@gmail.com>
2016-04-23 21:07:21 -0400

    Verification SoinRecuTest

--- test/inm5151/SoinRecuTest.java
+++ test/inm5151/SoinRecuTest.java
@@ -13,40 +13,40 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- *@author Serge Dogny
+ * @author Serge Dogny
  * @author ALLAMOU Fatima-Ezzahra
- * @author MAKHIVCHTCHOUK  Olga
+ * @author MAKHIVCHTCHOUK Olga
  */
 public class SoinRecuTest {
-    
+
     SoinRecu soin1;
     SoinRecu soin2;
     SoinRecu soin3;
     SoinRecu soin4;
-    
+
     public SoinRecuTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
-        
+
         soin1 = new SoinRecu("100", "2016-04-04", "180.00$", "CXS4567");
         soin2 = new SoinRecu("200", "2016-03-04", "230.00$", "DSJ1249");
         soin3 = new SoinRecu();
         soin4 = new SoinRecu("990", "2016-04-04", "16.50$", "KGB1079");
     }
-    
+
     @After
     public void tearDown() {
-        
+
         soin1 = null;
         soin2 = null;
         soin3 = null;
@@ -58,7 +58,7 @@ public class SoinRecuTest {
      */
     @Test
     public void testGetNumSoin() {
-        
+
         assertEquals("100", soin1.getNumSoin());
         assertNull(soin3.getNumSoin());
         assertEquals("200", soin2.getNumSoin());
@@ -69,7 +69,7 @@ public class SoinRecuTest {
      */
     @Test
     public void testGetDatesoin() {
-        
+
         assertEquals("2016-04-04", soin1.getDatesoin());
         assertNull(soin3.getDatesoin());
         assertEquals("2016-04-04", soin4.getDatesoin());
@@ -80,12 +80,11 @@ public class SoinRecuTest {
      */
     @Test
     public void testGetMontant() {
-        
+
         assertEquals("180.00$", soin1.getMontant());
         assertNull(soin3.getMontant());
         assertEquals("230.00$", soin2.getMontant());
-        
-        
+
     }
 
     /**
@@ -93,44 +92,44 @@ public class SoinRecuTest {
      */
     @Test
     public void testGetIdMedecin() {
-        
+
         assertEquals("CXS4567", soin1.getIdMedecin());
         assertNull(soin3.getIdMedecin());
         assertEquals("KGB1079", soin4.getIdMedecin());
     }
-    
+
     @Test
     public void testSetNumSoin() {
-        
+
         assertNull(soin3.getNumSoin());
         soin3.setNumSoin("200");
         assertNotNull(soin3.getNumSoin());
         assertEquals("200", soin3.getNumSoin());
-        
+
     }
-    
+
     @Test
     public void testSetDateSoin() {
-        
+
         assertNull(soin3.getDatesoin());
         soin3.setDateSoin("2016-11-23");
         assertNotNull(soin3.getDatesoin());
         assertEquals("2016-11-23", soin3.getDatesoin());
-        
+
     }
-    
+
     @Test
     public void testSetMontant() {
-        
+
         assertNull(soin3.getMontant());
         soin3.setMontant("234.56$");
         assertNotNull(soin3.getMontant());
         assertEquals("234.56$", soin3.getMontant());
     }
-    
+
     @Test
     public void testSetIdMedecin() {
-        
+
         assertNull(soin3.getIdMedecin());
         soin3.setIdMedecin("DGS7875");
         assertNotNull(soin3.getIdMedecin());
@@ -142,7 +141,7 @@ public class SoinRecuTest {
      */
     @Test
     public void testValiderNumeroSoin() {
-        
+
         assertTrue(soin1.validerNumeroSoin());
         assertFalse(soin4.validerNumeroSoin());
         assertTrue(soin2.validerNumeroSoin());
@@ -153,7 +152,7 @@ public class SoinRecuTest {
      */
     @Test
     public void testValiderLaDateDuSoin() {
-        
+
         assertTrue(soin1.validerLaDateDuSoin());
         soin3.setDateSoin("2016-13-18");
         assertFalse(soin3.validerLaDateDuSoin());
@@ -165,7 +164,7 @@ public class SoinRecuTest {
      */
     @Test
     public void testValiderMontant() {
-        
+
         assertTrue(soin1.validerMontant());
         soin3.setMontant("345.00");
         assertFalse(soin3.validerMontant());
@@ -179,7 +178,7 @@ public class SoinRecuTest {
      */
     @Test
     public void testValiderIdMedecin() {
-        
+
         assertTrue(soin1.validerIdMedecin());
         soin3.setIdMedecin("DRH87");
         assertFalse(soin3.validerLaDateDuSoin());
@@ -193,8 +192,11 @@ public class SoinRecuTest {
      */
     @Test
     public void testToString() {
-        
+
         String result = soin1.toString();
         assertEquals(result, soin1.toString());
+        
+        result = soin2.toString();
+        assertEquals(result, soin2.toString());
     }
 }
